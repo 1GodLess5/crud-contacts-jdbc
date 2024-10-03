9e8e1fc703707ad32f80713886a50b03787b0de6
1GodLess5 <gacekalbert@seznam.cz>
2024-10-03 23:24:44 +0200

    Changes for connection ty MySQL db.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import cz.godless.HikariCPDataSource;
+
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String select = "SELECT * FROM contact";
+
+        try (Connection connection = HikariCPDataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(select);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String phoneNumber = rs.getString("phone");
+
+                System.out.println("id: " + id + ", name " + name + ", email " + email + ", phone number: " + phoneNumber);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error while connecting to database!");
+        }
     }
 }

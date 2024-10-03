a0f2935c1cf169cb2855dd6c51955d540c9c460c
1GodLess5 <gacekalbert@seznam.cz>
2024-10-04 00:01:52 +0200

    Refactor of Main class.

--- src/main/java/cz/godless/Main.java
+++ src/main/java/cz/godless/Main.java
@@ -1,28 +1,10 @@
 package cz.godless;
 
-import cz.godless.HikariCPDataSource;
-
-import java.sql.*;
+import cz.godless.db.DBContactService;
 
 public class Main {
     public static void main(String[] args) {
-        String select = "SELECT * FROM contact";
-
-        try (Connection connection = HikariCPDataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement(select);
-             ResultSet rs = ps.executeQuery()) {
-
-            while (rs.next()) {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                String email = rs.getString("email");
-                String phoneNumber = rs.getString("phone");
-
-                System.out.println("id: " + id + ", name " + name + ", email " + email + ", phone number: " + phoneNumber);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Error while connecting to database!");
-        }
+        DBContactService service = new DBContactService();
+        service.readAll().forEach(System.out::println);
     }
 }

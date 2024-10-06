78b6cf63156b4bb2d490cebfd60a6b007d09e03a
1GodLess5 <gacekalbert@seznam.cz>
2024-10-06 23:11:31 +0200

    implemented Add contacts, new method -> createContact()

--- src/main/java/cz/godless/service/CRUDManager.java
+++ src/main/java/cz/godless/service/CRUDManager.java
@@ -28,7 +28,7 @@ public class CRUDManager {
             switch (choice) {
                 case 0 -> printAllContacts();
                 case 1 -> System.out.println("To be implemented.");
-                case 2 -> System.out.println("To be implemented.");
+                case 2 -> createContact();
                 case 3 -> System.out.println("To be implemented.");
                 case 4 -> System.out.println("To be implemented.");
                 case 5 -> {
@@ -37,6 +37,21 @@ public class CRUDManager {
                 }
                 default -> System.out.println("Invalid choice!");
             }
+        }
+    }
+
+    private void createContact() {
+        System.out.println("Enter name: ");
+        final String name = InputUtils.readString();
+        System.out.println("Enter email: ");
+        final String email = InputUtils.readString();
+        System.out.println("Enter phone: ");
+        final String phone = InputUtils.readString();
+
+        if (contactService.create(name, email, phone) > 0) {
+            System.out.println("Contact created successfully.");
+        } else {
+            System.out.println("Could not create contact.");
         }
     }
 

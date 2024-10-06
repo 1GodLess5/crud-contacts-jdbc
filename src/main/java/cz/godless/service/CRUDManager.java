6425f46be41603f6b38d6b9e90ef9d8aef5f964e
1GodLess5 <gacekalbert@seznam.cz>
2024-10-06 22:51:13 +0200

    created constructor, new methods: printOptions(), printAllContacts()

--- src/main/java/cz/godless/service/CRUDManager.java
+++ src/main/java/cz/godless/service/CRUDManager.java
@@ -1,4 +1,47 @@
 package cz.godless.service;
 
+import cz.godless.db.Contact;
+import cz.godless.db.DBContactService;
+import cz.godless.utility.InputUtils;
+
+import java.util.List;
+
 public class CRUDManager {
+    private final DBContactService contactService;
+
+    public CRUDManager () {
+        this.contactService = new DBContactService();
+    }
+
+    public void printOptions() {
+        System.out.println("Hello, welcome to contacts manager\n");
+
+        while (true) {
+            System.out.println("0. Get all contacts");
+            System.out.println("1. Edit contacts");
+            System.out.println("2. Add contacts");
+            System.out.println("3. Delete contacts");
+            System.out.println("4. Search contacts");
+            System.out.println("5. Exit");
+
+            final int choice = InputUtils.readInt();
+            switch (choice) {
+                case 0 -> printAllContacts();
+                case 1 -> System.out.println("To be implemented.");
+                case 2 -> System.out.println("To be implemented.");
+                case 3 -> System.out.println("To be implemented.");
+                case 4 -> System.out.println("To be implemented.");
+                case 5 -> {
+                    System.out.println("Good bye!");
+                    return;
+                }
+                default -> System.out.println("Invalid choice!");
+            }
+        }
+    }
+
+    private void printAllContacts() {
+        final List<Contact> contacts = contactService.readAll();
+        contacts.forEach(System.out::println);
+    }
 }
